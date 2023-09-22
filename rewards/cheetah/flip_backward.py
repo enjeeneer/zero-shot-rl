51def28cf6f329cdfffe5fda3d9cae9a4192f7b7
enjeeneer <scottjeen@gmail.com>
2023-09-22 14:26:00 +0100

    delete unneeded files

--- rewards/cheetah/flip_backward.py
+++
@@ -1,8 +0,0 @@
-"""
-module for retrieving reach_bottom_left reward function
-from dm_control suite point_mass_maze
-"""
-
-from custom_dmc_tasks.cheetah import flip_backward
-
-reward_function = flip_backward()._task.get_reward  # pylint: disable=protected-access
--- rewards/cheetah/run.py
+++
@@ -1,9 +0,0 @@
-"""
-module for retrieving reach_bottom_left reward function
-from dm_control suite point_mass_maze
-"""
-
-from custom_dmc_tasks.cheetah import make
-
-env = make(task="run")
-reward_function = env._task.get_reward  # pylint: disable=protected-access
